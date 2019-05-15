1dee514ec913432b02c7bcd5dd56b9db22bda424
loveyghotraa <sing9184@sheridancollege.ca>
2019-05-15 14:36:29 -0400

    Student demo program

---
+++ src/studenttemplate/StudentTemplate.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studenttemplate;
+
+/**
+ *
+ * @author lovey
+ */
+
+public class StudentTemplate
+{/**
+     * @param args the command line argument*/
+    private String name;//encapsulation
+    
+        // TODO code application logic here
+
+    /**
+     * @return the name
+     */
+    //constructors
+    public StudentTemplate(){
+    
+}
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
---
+++ src/studenttemplate/StudentTester.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studenttemplate;
+
+
+
+/**
+ *
+ * @author lovey
+ */
+public class StudentTester {
+    public static void main(String[] args) {
+    StudentTemplate s1=new StudentTemplate(); 
+    s1.setName("lovepreet");
+    //System.out.println(s1.getName());
+    StudentTemplate s2=new StudentTemplate(); 
+    s2.setName("singh");
+    //System.out.println(s2.getName());
+    StudentTemplate[] list=new StudentTemplate[2];//Array of objects
+    list[0]=s1;
+    list[1]=s2;
+    for(int i=0; i<list.length;i++)
+    {
+        System.out.println(list[i].getName());
+    }
+    }
+    
+}
